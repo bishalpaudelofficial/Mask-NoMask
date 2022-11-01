8035b1647b160ea61504c8ccaae12426f6b39473
Hancy-22 <62088646+Bishal1022@users.noreply.github.com>
2022-11-02 03:02:43 +0530

    Delete maskrecognizer.m

--- maskrecognizer.m
+++
@@ -1,9 +0,0 @@
-function [masklabel,queryFeatures] = maskrecognizer(queryimage,maskClassifier)
-%maskdetector detect person is wearing mask or not
-%   Detailed explanation goes here
-
-queryFeatures = extractHOGFeatures(queryimage,'CellSize',[4 4]);
-masklabel = predict(maskClassifier,queryFeatures);
-
-end
-
