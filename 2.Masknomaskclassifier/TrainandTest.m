36342deee15d747d27fa9ec06a273b5ee23b8f63
Hancy-22 <62088646+Bishal1022@users.noreply.github.com>
2022-11-02 02:41:26 +0530

    Add files via upload

---
+++ 2.Masknomaskclassifier/TrainandTest.m
@@ -0,0 +1,65 @@
+%% Refresher
+%With Regards Team Bazinga ;)
+clc;
+close all;
+clear;
+%% Read Training Data 
+faceGallery = imageSet('TrainingData', 'recursive');
+galleryNames = {faceGallery.Description};
+%% Create HoG training features from Training data
+trainingFeatures = zeros(1,46656);
+featureCount = 1;
+
+for i=1:size(faceGallery,2)
+    for j = 1:faceGallery(i).Count
+        sizeNormalizedImage = imresize(read(faceGallery(i),j),[150 150]);
+        trainingFeatures(featureCount,:) = extractHOGFeatures(sizeNormalizedImage,'CellSize',[4 4]);
+        trainingLabel{featureCount} = faceGallery(i).Description;    
+        featureCount = featureCount + 1;
+    end
+    personIndex{i} = faceGallery(i).Description;
+end
+
+%% Train A Multiclass SVM Classifier Using HOG Features
+maskClassifier = fitcecoc(trainingFeatures,trainingLabel);
+save('utilities\maskClassifier.mat','maskClassifier','-v7.3');
+%% Test video
+% vidobj =  vision.VideoFileReader('data\nomask.mp4');%Name of the video
+% viewer = vision.DeployableVideoPlayer;
+% load(fullfile("utilities","detectorYoloV2.mat"));
+% load(fullfile("utilities","faceClassifier1.mat"));
+% position =  [1 20];
+% value = 'Processing...';
+% counter=1;
+% personLabel={};
+% while ~isDone(vidobj)
+%     image=step(vidobj);
+%     [bboxes,scores,labels] = detect(detectorYolo2,image);
+%     image = insertText(image,position,value,'AnchorPoint','LeftBottom');
+%     if ~isempty(bboxes) 
+%      image= insertShape(image,'rectangle', bboxes,'Linewidth',3);
+%      crop = imcrop(image,bboxes(1,:));    
+%      gray1=rgb2gray(crop);
+%      queryimage=imresize(gray1,[150 150]);
+%      ab=bboxes(1,3)*bboxes(1,4);
+%        if counter>=1 && ab>5000
+%        queryFeatures = extractHOGFeatures(queryimage,'CellSize',[4 4]);
+%        personLabel = predict(maskClassifier,queryFeatures);
+%        counter=counter-1;
+%        end
+%     end
+%     if ~isempty(personLabel)
+%         a=personLabel{1};
+%         a=size(a);
+%         if a(1,2)==4
+%             a='Mask detected';  
+%         elseif a(1,2)==6
+%             a='Wear Mask Save Life';
+%         end    
+%       image = insertText(image,[40 45],a,'AnchorPoint','LeftBottom');
+%     end
+% 
+%     step(viewer, image);
+% end
+% 
+% release(viewer);
